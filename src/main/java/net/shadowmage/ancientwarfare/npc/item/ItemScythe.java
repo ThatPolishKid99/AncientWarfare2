8f054d4021d9c04d37fa703763bcaace134731bb
WinDanesz <win.danesz@users.noreply.github.com>
2019-09-18 23:01:09 +0100

    Added sickle, scythe and pitchfork. optimized ice spear and giant club code

--- src/main/java/net/shadowmage/ancientwarfare/npc/item/ItemScythe.java
+++ src/main/java/net/shadowmage/ancientwarfare/npc/item/ItemScythe.java
@@ -2,86 +2,125 @@ package net.shadowmage.ancientwarfare.npc.item;
 
 import com.google.common.collect.Multimap;
 import net.minecraft.block.Block;
-import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.MobEffects;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.common.IPlantable;
-import net.shadowmage.ancientwarfare.core.util.BlockTools;
 import net.shadowmage.ancientwarfare.npc.AncientWarfareNPC;
+import net.shadowmage.ancientwarfare.core.util.BlockTools;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
 
 public class ItemScythe extends ItemBaseNPC {
 	private final ToolMaterial material;
 
 	private final float attackDamage;
 	protected float attackSpeed;
-	private static final int HARVEST_RADIUS = 2;
+	private final int harvestRadius;
+	private final Set<BlockPos> blocksToHarvest = new LinkedHashSet<>();
 
 	public ItemScythe(ToolMaterial material, String registryName, float attackOffset, float attackSpeed) {
 		super(registryName);
 		this.material = material;
-		setMaxStackSize(1);
-		setMaxDamage(material.getMaxUses());
-		attackDamage = material.getAttackDamage() + attackOffset;
+		this.maxStackSize = 1;
+		this.setMaxDamage(material.getMaxUses());
+		this.attackDamage = material.getAttackDamage() + attackOffset;
 		this.attackSpeed = attackSpeed;
 		setUnlocalizedName(registryName);
 		setCreativeTab(AncientWarfareNPC.TAB);
+		this.harvestRadius = 3;
 	}
 
-	@Override
-	public int getItemEnchantability() {
+	public float getAttackDamage()
+	{
+		return this.material.getAttackDamage();
+	}
+
+	public int getItemEnchantability()
+	{
 		return this.material.getEnchantability();
 	}
 
 	@Override
-	public Multimap<String, AttributeModifier> getAttributeModifiers(EntityEquipmentSlot slot, ItemStack stack) {
-		Multimap<String, AttributeModifier> multimap = super.getAttributeModifiers(slot, stack);
+	public Multimap<String, AttributeModifier> getItemAttributeModifiers(EntityEquipmentSlot equipmentSlot)
+	{
+		Multimap<String, AttributeModifier> multimap = super.getItemAttributeModifiers(equipmentSlot);
 
-		if (slot == EntityEquipmentSlot.MAINHAND) {
-			multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.getName(), new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Weapon modifier", (double) this.attackDamage, 0));
-			multimap.put(SharedMonsterAttributes.ATTACK_SPEED.getName(), new AttributeModifier(ATTACK_SPEED_MODIFIER, "Weapon modifier", attackSpeed, 0));
+		if (equipmentSlot == EntityEquipmentSlot.MAINHAND)
+		{
+			multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.getName(), new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Weapon modifier", (double)this.attackDamage, 0));
+			multimap.put(SharedMonsterAttributes.ATTACK_SPEED.getName(), new AttributeModifier(ATTACK_SPEED_MODIFIER, "Weapon modifier", attackSpeed , 0));
 		}
 		return multimap;
 	}
 
-	@SuppressWarnings("squid:S1186") //used in child class
-	protected void applyPotionEffect(EntityLivingBase target) {
-	}
-
 	@Override
 	public boolean hitEntity(ItemStack stack, EntityLivingBase target, EntityLivingBase attacker) {
-		applyPotionEffect(target);
-		return true;
+		if (this.getRegistryName().toString().equals("ancientwarfarenpc:death_scythe")){
+			target.addPotionEffect(new PotionEffect(MobEffects.WITHER, 100));
+		}
+			return true;
 	}
 
 	@Override
-	public boolean onBlockStartBreak(ItemStack itemstack, BlockPos pos, EntityPlayer player) {
-		World world = player.world;
-		boolean harvestedSomething = false;
-		for (BlockPos currPos : BlockPos.getAllInBox(pos.add(-HARVEST_RADIUS, -HARVEST_RADIUS, -HARVEST_RADIUS), pos.add(HARVEST_RADIUS, HARVEST_RADIUS, HARVEST_RADIUS))) {
-			IBlockState currState = world.getBlockState(currPos);
-			Block currBlock = currState.getBlock();
-
+	public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+		List<BlockPos> blocksToCheck = getBlocksToCheck(pos);
+		BlockPos currPos;
+		// get the list of blocks to check
+		for (int i = 0; i < blocksToCheck.size(); i++) {
+			currPos = blocksToCheck.get(i);
+			Block currBlock = worldIn.getBlockState(currPos).getBlock();
+			// add only the farmable blocks to the list
 			if (isFarmable(currBlock)) {
-				harvestedSomething = true;
-
-				BlockTools.breakBlockAndDrop(world, currPos);
-				world.playEvent(player, 2001, currPos, Block.getStateId(currState));
+				blocksToHarvest.add(currPos);
 			}
 		}
-		if (!harvestedSomething) {
-			return false;
+		Iterator<BlockPos> it = blocksToHarvest.iterator();
+		if (blocksToHarvest.isEmpty()) {
+			return EnumActionResult.FAIL;
 		}
-		player.getHeldItemMainhand().damageItem(2, player);
-		return true;
+		it.forEachRemaining(t -> BlockTools.breakBlockAndDrop(worldIn, t));	// harvest blocks
+		worldIn.playSound(player, pos, SoundEvents.BLOCK_GRASS_BREAK, SoundCategory.BLOCKS, 1.0F, 1.0F);
+		blocksToHarvest.clear();
+		// damage the stack
+		ItemStack item = player.getHeldItem(hand);
+		item.damageItem(2, player);
+		return EnumActionResult.SUCCESS;
 	}
 
 	private boolean isFarmable(Block block) {
 		return block instanceof IPlantable;
+	}
+
+	public List<BlockPos> getBlocksToCheck(BlockPos pos) {
+		List<BlockPos> blocksToCheck = new ArrayList<>();
+		blocksToCheck.add(pos);
+		if (harvestRadius == 3) {
+			blocksToCheck.add(pos.north());
+			blocksToCheck.add(pos.north().east());
+			blocksToCheck.add(pos.north().west());
+			blocksToCheck.add(pos.south());
+			blocksToCheck.add(pos.south().east());
+			blocksToCheck.add(pos.south().west());
+			blocksToCheck.add(pos.east());
+			blocksToCheck.add(pos.west());
+		}
+		return blocksToCheck;
 	}
 }
