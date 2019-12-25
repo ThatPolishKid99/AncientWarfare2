2dfd932ac6cfa0a6e6ff44cde38c350db0728ab8
WinDanesz <win.danesz@users.noreply.github.com>
2019-12-25 15:19:10 +0100

    Added a new GUI option and NPC property to allow faction NPCs to retain custom equipments instead of the faction defaults.

--- src/main/java/net/shadowmage/ancientwarfare/npc/gui/GuiNpcCreativeControls.java
+++ src/main/java/net/shadowmage/ancientwarfare/npc/gui/GuiNpcCreativeControls.java
@@ -10,6 +10,7 @@ import net.shadowmage.ancientwarfare.core.gui.elements.Label;
 import net.shadowmage.ancientwarfare.core.gui.elements.NumberInput;
 import net.shadowmage.ancientwarfare.core.gui.elements.Text;
 import net.shadowmage.ancientwarfare.npc.container.ContainerNpcCreativeControls;
+import net.shadowmage.ancientwarfare.npc.entity.faction.NpcFaction;
 
 public class GuiNpcCreativeControls extends GuiContainerBase<ContainerNpcCreativeControls> {
 	private Text ownerNameInput;
